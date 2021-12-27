26f4dd49896ee58ad2accb5865a7ae4e9f6eddf5
Nurbk229 <mrh61037@gmail.com>
2021-12-27 05:28:27 -0800

    finished media player for Home View Controller "Azker M and AzKer S"

--- azkaree/viewConrollers/MalaViewController.swift
+++ azkaree/viewConrollers/MalaViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 
+
 class MalaViewController: UIViewController {
 
     @IBOutlet weak var counter:UIButton!
@@ -29,14 +30,6 @@ class MalaViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
